059126b4fb462542184717f752a3b4dc1bb34136
Scott Molinari <scott.molinari@adduco.de>
2022-04-06 07:58:01 +0200

    Get things running.

--- src/__demo.ts
+++ src/__demo.ts
@@ -1,21 +1,20 @@
-import { CanActivate, ExecutionContext, Injectable, NestMiddleware } from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
 import type { FastifyReply, FastifyRequest } from "fastify";
 
-@Injectable()
-export class DemoMiddleware implements NestMiddleware {
-  async use(req: FastifyRequest, _: FastifyReply, next: () => void): Promise<void> {
-    console.log("middleware has run")
-    req.user = { exists: true };
-    console.log("attached user to request")
-    next();
-  }
+export function demoMiddleware (req: FastifyRequest, _: FastifyReply, next: () => void): void {
+  console.log("middleware has run")
+  req.user = { exists: true };
+  console.log("attached user to request")
+  next();
 }
 
 @Injectable()
 export class DemoGuard implements CanActivate {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest<FastifyRequest>();
-    console.log("GUARD USER", req.user);
+  async canActivate (context: ExecutionContext): Promise<boolean> {
+    const ctx = GqlExecutionContext.create(context);
+    const req = ctx.getContext().req;
+    console.log("GUARD USER", req.raw.user);
     return true;
   }
 
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { demoMiddleware } from './__demo';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   app.useGlobalPipes(new ValidationPipe());
-
+  app.use(demoMiddleware)
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
